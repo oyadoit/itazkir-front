121ac255196666f557675613b70b8c20b5236545
Amin Ajao <Aminajao96@gmail.com>
2021-04-11 20:31:58 +0100

    fix: layout error

--- src/Components/Pages/Content.jsx
+++ src/Components/Pages/Content.jsx
@@ -8,7 +8,7 @@ import Header from '../Custom/Header';
 import SmallReminderCard from '../Custom/SmallReminderCard';
 
 const Content = (props) => {
-const id = useParams('reminderName')
+  const id = useParams('reminderName')
   const { loading, data } = useQuery(REMINDER_CONTENT, {
     variables: {
       reminderId: id.reminderName
@@ -18,42 +18,42 @@ const id = useParams('reminderName')
 
   if (loading) {
     return (
-    <div
-      style={{
-        margin: "Auto",
-        marginTop: "60px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Spin size="large" />
-    </div>
-     ) 
-    }
-   
+      <div
+        style={{
+          margin: "Auto",
+          marginTop: "60px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spin size="large" />
+      </div>
+    )
+  }
+
 
   return (
     <div className="content__page--container">
       <Header />
       <Heading>Daily Reminder</Heading>
-      {data.reminderContent && data.reminderContent.map(content => 
       <Container>
-        <SmallReminderCard
-          title={`${content.data}`}
-          content="This is part of the content for the reminder"
-          bgColor="#fff"
-          tag="Islamic Reminder"
-          //   by={`${eachContent.reminder.owner.firstName}  ${eachContent.reminder.owner.lastName}`}
-          by="I R"
-          key="1"
-          id="1"
-          ownerId="21"
-          to={`/reminders/${id.reminderName}/content/${content.id}`}
-        />
+        {data.reminderContent && data.reminderContent.map(content =>
+          <SmallReminderCard
+            title={`${content.data}`}
+            content="This is part of the content for the reminder"
+            bgColor="#fff"
+            tag="Islamic Reminder"
+            //   by={`${eachContent.reminder.owner.firstName}  ${eachContent.reminder.owner.lastName}`}
+            by="I R"
+            key="1"
+            id="1"
+            ownerId="21"
+            to={`/reminders/${id.reminderName}/content/${content.id}`}
+          />
+        )}
       </Container>
-      )}
-      
+
     </div>
   );
 };
